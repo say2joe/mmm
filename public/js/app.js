8e6a5bc84f4e49b3a1ad1900bf0b12401dc5a542
Joe Johnson <say2joe@gmail.com>
2014-06-17 21:11:47 -0700

    Updates to JS

--- public/js/app.js
+++ public/js/app.js
@@ -2,3 +2,15 @@
 
 angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.router', 'mean.system']);
 angular.module('mean.system', []);
+
+// Setup global variables
+var app = app || angular, MMM = MMM || {
+    title: "Mobile Majority (with MEAN)"
+};
+
+// Use module pattern for scope
+(function(ng, ns, global, undefined) {
+    
+    debugger;
+    
+})(app, MMM, window);
