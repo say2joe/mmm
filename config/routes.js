f67db89d24175de61236c1134b853ae6dffbaa37
Joe Johnson <say2joe@gmail.com>
2014-06-18 14:02:06 -0700

    Updated: Routes and Controllers

--- config/routes.js
+++ config/routes.js
@@ -1,31 +1,10 @@
-var async = require('async'),
-    AdModel = require('../app/models/ad'),
-    AdClickModel = require('../app/models/adclick');
+var async = require('async');
 
 module.exports = function(app) {
-    var index = require('../app/controllers/index');
+    var index = require('../app/controllers/index'),
+        ads = require('../app/controllers/ads');
     
-    app.get('/ads/:id', function(req, res) {
-        var id = req.params.id;
-        AdClickModel.create({
-                ad_id: id,
-                time_stamp: (new Date()),
-                referrer: req.get('Referer')
-                // Using actual misspelled header
-            },
-            function(err, adclick) {
-                if (err) res.send(err);
-                AdClickModel.find(
-                    { ad_id: adclick.ad_id },
-                    function(err, adclicks) {
-                        if (err) res.send(err);
-                        console.log(adclicks);
-                        res.redirect('/');
-                    }
-                );
-            }
-        );
-    });
+    app.get('/ads/:id', ads.tracking);
     
     app.get('*', index.render);
 };
