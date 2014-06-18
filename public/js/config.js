7534765ce7ff7b3a1af911d09d96cc17cbeba306
Joe Johnson <say2joe@gmail.com>
2014-06-18 16:11:44 -0700

    Updated: D3

--- public/js/config.js
+++ public/js/config.js
@@ -2,22 +2,21 @@
 
 //Setting up route
 angular.module('mean').config(['$stateProvider', '$urlRouterProvider',
-  function($stateProvider, $urlRouterProvider) {
-    // For unmatched routes:
-    $urlRouterProvider.otherwise('/');
+    function($stateProvider, $urlRouterProvider) {
+        // For unmatched routes:
+        $urlRouterProvider.otherwise('/');
 
-    // states for my app
-    $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'views/index.html'
-    });
-}
+        // states for my app
+        $stateProvider.state('home', {
+            url: '/',
+            templateUrl: 'views/index.html'
+        });
+    }
 ]);
 
 //Setting HTML5 Location Mode
 angular.module('mean').config(['$locationProvider',
-  function($locationProvider) {
-    $locationProvider.hashPrefix('!');
-}
+    function($locationProvider) {
+        $locationProvider.hashPrefix('!');
+    }
 ]);
